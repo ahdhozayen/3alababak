4cd4ff660d15a90d42f1aff25e273b0538f5030d
rohanda <rohanda.hamed@mashreqarabia.com>
2020-08-05 17:06:42 +0200

    edit create-supplier view

--- account/views.py
+++ account/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render, redirect
 from account.forms import (CustomerCreationForm, SupplierCreationForm, AddressCreationForm, customer_address_formset,
-                            supplier_address_formset, CompanyCreationForm,)
+                           supplier_address_formset, CompanyCreationForm, )
 from django.forms import inlineformset_factory
 from account.models import Customer, Supplier, Address
 from django.contrib import messages
-
-
-def list_suppliers_view(request):
-    supliers_list = Supplier.objects.all()
-    supContext = {
-        'supliers_list': supliers_list,
-    }
-    return render(request, 'list-suppliers.html', supContext)
-
-def list_customer_view(request):
-    customers_list = Customer.objects.all()
-    supContext = {
-        'customers_list': customers_list,
-    }
-    return render(request, 'list-customers.html', supContext)
 
 
 def create_customer_address_account(request):
@@ -52,60 +37,46 @@ def create_customer_address_account(request):
     return render(request, 'create-supplier.html', context=customer_information_context)
 
 
-def create_customer(request):
-    customer_instance = None
-    if request.method == 'POST':
-        form = CustomerCreationForm(request.POST)
-        if form.is_valid():
-            form.save(commit=False)
-            form.created_by = request.user
-            form.last_updated_by = request.user
-            customer_instance = form.save()
-    else:
-        form = CustomerCreationForm()
-    return form, customer_instance
-
-
 def create_supplier_address_account(request):
-    form, supp = create_supplier(request)
-    address = create_address(request, supp, supplier_address_formset)
-    req_form = {
-        'account_form': form,
-        'address_form': address
+    if request.method == 'POST':
+        supplier_form = SupplierCreationForm(request.POST)
+        address_inlineformset = supplier_address_formset(request.POST)
+        if supplier_form.is_valid() and address_inlineformset.is_valid():
+            supplier_obj = supplier_form.save(commit=False)
+            supplier_obj.created_by = request.user
+            supplier_obj.company = request.user.company
+            supplier_instance = supplier_obj.save()
+            address_inlineformset = supplier_address_formset(request.POST, instance=supplier_instance)
+            for form in address_inlineformset:
+                if form.is_valid():
+                    address_obj = form.save(commit=False)
+                    address_obj.created_by = request.user
+                    address_obj.save()
+
+            messages.success(request, 'Saved Successfully')
+            return redirect('account:list-suppliers')
+        else:
+            print(supplier_form.errors)
+    else:
+        supplier_form = SupplierCreationForm()
+        address_inlineformset = supplier_address_formset()
+
+    supplier_information_context = {
+        'account_form': supplier_form,
+        'address_inlineformset': address_inlineformset
     }
 
-    return render(request, 'account.html', context=req_form)
+    return render(request, 'create-supplier.html', context=supplier_information_context)
 
 
-def create_supplier(request):
-    supplier_instance = None
-
+def create_company(request):
+    form = CompanyCreationForm()
     if request.method == 'POST':
-        form = SupplierCreationForm(request.POST)
+        form = CompanyCreationForm(request.POST)
         if form.is_valid():
-            form.save(commit=False)
-            form.created_by = request.user
-            form.last_updated_by = request.user
-            supplier_instance = form.save()
-    else:
-        form = SupplierCreationForm()
-    return form, supplier_instance
-
-
-def create_address(request, account_instance, address_formset):
-    forms = address_formset()
-    if request.method == 'POST':
-        if account_instance is None:
-            print("please enter valid account information")
-        else:
-            forms = address_formset(request.POST, instance=account_instance)
-            for form in forms:
-                if form.is_valid():
-                    form.save(commit=False)
-                    form.created_by = request.user
-                    form.last_updated_by = request.user
-                    form.save()
-    return forms
+            form.save()
+    req_form = {'company_form': form}
+    return render(request, 'company.html', context=req_form)
 
 
 def list_customers(request):
@@ -120,11 +91,17 @@ def list_suppliers(request):
     return render(request, 'list_suppliers.html', context=supcontext)
 
 
-def create_company(request):
-    form = CompanyCreationForm()
-    if request.method == 'POST':
-        form = CompanyCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-    req_form = {'company_form': form}
-    return render(request, 'company.html', context=req_form)
+def list_suppliers_view(request):
+    supliers_list = Supplier.objects.all()
+    supContext = {
+        'supliers_list': supliers_list,
+    }
+    return render(request, 'list-suppliers.html', supContext)
+
+
+def list_customer_view(request):
+    customers_list = Customer.objects.all()
+    supContext = {
+        'customers_list': customers_list,
+    }
+    return render(request, 'list-customers.html', supContext)
