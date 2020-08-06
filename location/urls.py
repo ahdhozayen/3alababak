0f574638750a5b89b2fe2dc101e0c52176b2fcc1
ahdhozayen <ahdhozayen@gmail.com>
2020-08-06 14:44:15 +0200

    location commit

---
+++ location/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from account import views
+
+app_name = 'location'
+urlpatterns = [
+    path('create/', views.create_location_view, name='create-location'),
+    path('update/', views.update_location_view, name='update-location'),
+    path('list/locations/', views.list_location_view, name='list-locations'),
+    path('delete/', views.delete_location_view, name='delete-location'),
+]
--- location/views.py
+++ location/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from location.models import Location
+from location.forms import LocationCreationForm
 
-# Create your views here.
+
+def list_location_view(request):
+    locations_list = Location.objects.all()
+    location_context = {
+                        'locations_list':locations_list,
+    }
+    return render(request, 'list-locations.html', location_context)
+
+
+def create_location_view(request):
+    if request.method == 'POST':
+        location_form = LocationCreationForm(request.POST)
+        if location_form.is_valid():
+            location_obj = location_form.save(commit=False)
+            location_obj.created_by = request.user
+            location_obj.company = request.user.company
+            location_obj.save()
+    else:
+        location_form = LocationCreationForm()
+    location_context = {
+                        'location_form':location_form,
+    }
+    return render(request, 'create-location.html', location_context)
+
+
+def update_location_view(request):
+    if request.method == 'POST':
+        location_form = LocationCreationForm(request.POST)
+        if location_form.is_valid():
+            location_obj = location_form.save(commit=False)
+            location_obj.created_by = request.user
+            location_obj.company = request.user.company
+            location_obj.save()
+    else:
+        location_form = LocationCreationForm()
+    location_context = {
+                        'location_form':location_form,
+    }
+    return render(request, 'create-location.html', location_context)
+
+
+def delete_location_view(request):
+    return redirect('location:list-locations')
