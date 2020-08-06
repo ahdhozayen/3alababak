ef4fc030504a2abe8ff39d73e9913accdcd03857
rohanda <rohanda.hamed@mashreqarabia.com>
2020-08-06 14:42:24 +0200

    add link to po in side bar

--- orders/forms.py
+++ orders/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from orders.models import PurchaseOder
 from django.forms import inlineformset_factory
 from orders.models import PurchaseOder, PurchaseTransaction
+from djmoney.models.fields import MoneyField
 
 
 class PurchaseOrderCreationForm(forms.ModelForm):
@@ -9,7 +10,16 @@ class PurchaseOrderCreationForm(forms.ModelForm):
         model = PurchaseOder
         exclude = ('created_at', 'last_updated_at', 'created_by', 'last_updated_by')
 
+    def __init__(self, *args, **kwargs):
+        super(PurchaseOrderCreationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'total_price':
+                self.fields[field].widget.attrs['class'] = 'form-control'
 
+            elif self.fields[field].widget.input_type == 'checkbox':
+                self.fields[field].widget.attrs['class'] = 'form-check-input'
+            else:
+                self.fields[field].widget.attrs['class'] = 'form-control'
 
 
 class PurchaseTransactionCreationForm(forms.ModelForm):
@@ -27,4 +37,4 @@ class PurchaseTransactionCreationForm(forms.ModelForm):
 
 
 purchase_transaction_formset = inlineformset_factory(PurchaseOder, PurchaseTransaction,
-                                                 form=PurchaseTransactionCreationForm, extra=3, can_delete=False)
+                                                     form=PurchaseTransactionCreationForm, extra=3, can_delete=False)
